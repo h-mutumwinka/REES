dbd99b0ff54ab861fa3a9e2606e17d6db8f5b280
h-mutumwinka <h.mutumwink@alustudent.com>
2025-11-27 00:03:18 +0200

    updating manage file

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 
 
 def main():
-    """Running tasks of adminisatration"""
+    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
     try:
         from django.core.management import execute_from_command_line
