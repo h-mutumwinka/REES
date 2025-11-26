712fdf35a5c0684fe9b92bd9ac123d3b7a76f78b
root <h.mutumwink@alustudent.com>
2025-11-26 15:15:55 +0200

    updating the login and signup files

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from education import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', include('education.urls')),  
 ]
